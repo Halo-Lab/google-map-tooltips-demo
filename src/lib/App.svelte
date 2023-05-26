644467183c236a121ab8f84a2933f50fbf6a5d80
Volodymyr K <vladimir.k@halo-lab.team>
2023-05-26 12:56:56 +0300

    overlapping

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -2,13 +2,43 @@
 	import { onDestroy, onMount, tick, createEventDispatcher } from 'svelte';
 	import type { MapMarkersType } from './types.d.ts';
 
+	type TElementPos = { left: number; top: number; bottom: number; right: number };
+
+	type TPopup = google.maps.OverlayView & {
+		hide: () => void;
+		show: () => void;
+		getPosition: (pos: google.maps.Point) => TElementPos;
+		mark: MapMarkersType;
+		popupsContainer: HTMLDivElement;
+		content: HTMLDivElement;
+		containerDiv: HTMLDivElement;
+	};
+
+	interface ClassType<InstanceType extends {} = {}> extends Function {
+		preventMapHitsAndGesturesFrom(containerDiv: HTMLDivElement): unknown;
+		new (...args: any[]): InstanceType;
+		prototype: InstanceType;
+	}
+
+	type IPopup = ClassType<TPopup>;
+
+	function elementsOverlap(pos1: TElementPos, pos2: TElementPos) {
+		if (!pos1 || !pos2) return false;
+		return !(
+			pos1.top > pos2.bottom ||
+			pos1.right < pos2.left ||
+			pos1.bottom < pos2.top ||
+			pos1.left > pos2.right
+		);
+	}
+
 	const dispatch = createEventDispatcher();
 
 	export let map: google.maps.Map;
 	export let markers: MapMarkersType[];
-	const popup: Map<string, google.maps.OverlayView> = new Map();
+	const popup: Map<string, TPopup> = new Map();
 	let popupsContainer: HTMLDivElement;
-	let Popup: any;
+	let Popup: IPopup;
 
 	function mapMoves(bounds: google.maps.LatLngBounds | undefined) {
 		dispatch('mapMove', { bounds });
@@ -38,12 +68,13 @@
 		const popupToDelete: Set<string> = new Set(popup.keys());
 
 		// Add new popups
-		markers.forEach((mark, i) => {
+		markers.forEach((mark) => {
 			// Remove from popup SET of ids to delete
 			popupToDelete.delete(mark.id);
 			if (popup.has(mark.id)) return;
 			// Create new popup id it doesn't exist
 			const newPopup = new Popup(mark, popupsContainer);
+
 			popup.set(mark.id, newPopup);
 			newPopup.setMap(map);
 		});
@@ -54,6 +85,34 @@
 				popup.get(id)?.onRemove();
 				popup.delete(id);
 			});
+		}
+
+		const popupCopy = new Map(popup);
+
+		for (const [key1, value1] of popupCopy) {
+			for (const [key2, value2] of popupCopy) {
+				if (!value1.getProjection()) continue;
+				if (key1 === key2) continue;
+				if (
+					elementsOverlap(
+						value1.getPosition(
+							value1.getProjection().fromLatLngToDivPixel(value1.mark.coordinates)!
+						),
+						value2.getPosition(
+							value2.getProjection().fromLatLngToDivPixel(value2.mark.coordinates)!
+						)
+					)
+				) {
+					console.log(value1.mark.name, '---overlaping-->', value2.mark.name);
+					if (value1.mark.priority > value2.mark.priority) {
+						popup.get(key2)?.hide();
+						popupCopy.delete(key1);
+					} else {
+						popup.get(key1)?.hide();
+						popupCopy.delete(key2);
+					}
+				}
+			}
 		}
 	}
 
@@ -95,7 +154,7 @@
 
 				this.containerDiv.classList.add('cursor-auto', 'h-0', 'absolute');
 
-				this.containerDiv.appendChild(this.content);
+				this.content = this.containerDiv.appendChild(this.content);
 
 				// Optionally stop clicks, etc., from bubbling up to the map.
 				Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
@@ -111,6 +170,26 @@
 				if (this.containerDiv.parentElement) {
 					this.containerDiv.parentElement.removeChild(this.containerDiv);
 				}
+			}
+
+			hide() {
+				this.content.classList.add('invisible');
+			}
+
+			show() {
+				this.content.classList.add('visible');
+			}
+			getPosition(pos: google.maps.Point) {
+				const h = 25 / 2;
+				const w = 43 / 2;
+				const { x, y } = pos;
+
+				return {
+					left: x - w,
+					top: y - h,
+					bottom: y + h,
+					right: x + w
+				};
 			}
 
 			/** Called each frame when the popup needs to draw itself. */
